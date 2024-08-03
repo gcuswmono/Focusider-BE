6e40d6edd4692c68873f99170a48e50ded0bfa84
ManchanTime <lych0918@gachon.ac.kr>
2024-08-03 21:33:29 +0900

    FK-29-feat 사용자 로그인 기능 구현

--- src/main/java/mono/focusider/domain/auth/dto/info/AuthUserInfo.java
+++ src/main/java/mono/focusider/domain/auth/dto/info/AuthUserInfo.java
@@ -3,6 +3,7 @@ package mono.focusider.domain.auth.dto.info;
 import lombok.AccessLevel;
 import lombok.Builder;
 import mono.focusider.domain.member.domain.Member;
+import mono.focusider.domain.member.type.MemberRole;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
@@ -14,19 +15,32 @@ public record AuthUserInfo(
         Long memberId,
         String name,
         String gender,
-        Integer level
+        Integer level,
+        MemberRole memberRole
 ) {
-    public static AuthUserInfo ofToken(Long memberId, String name, String gender, Integer level) {
+    public static AuthUserInfo of(Member member) {
+        return AuthUserInfo
+                .builder()
+                .memberId(member.getId())
+                .name(member.getName())
+                .gender(member.getGender())
+                .level(member.getLevel())
+                .memberRole(member.getMemberRole())
+                .build();
+    }
+
+    public static AuthUserInfo ofToken(Long memberId, String name, String gender, Integer level, MemberRole memberRole) {
         return AuthUserInfo
                 .builder()
                 .memberId(memberId)
                 .name(name)
                 .gender(gender)
                 .level(level)
+                .memberRole(memberRole)
                 .build();
     }
 
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton(new SimpleGrantedAuthority(null));
+        return Collections.singleton(new SimpleGrantedAuthority(memberRole.getDesc()));
     }
 }
---
+++ src/main/java/mono/focusider/domain/member/helper/MemberHelper.java
@@ -0,0 +1,21 @@
+package mono.focusider.domain.member.helper;
+
+import lombok.RequiredArgsConstructor;
+import mono.focusider.domain.member.domain.Member;
+import mono.focusider.domain.member.error.MemberErrorCode;
+import mono.focusider.domain.member.repository.MemberRepository;
+import mono.focusider.global.error.exception.EntityNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Component
+@Transactional(readOnly = true)
+public class MemberHelper {
+    private final MemberRepository memberRepository;
+
+    public Member findMemberByAccountIdOrThrow(String accountId) {
+        return memberRepository.findByAccountId(accountId)
+                .orElseThrow(() -> new EntityNotFoundException(MemberErrorCode.MEMBER_NOT_FOUND));
+    }
+}

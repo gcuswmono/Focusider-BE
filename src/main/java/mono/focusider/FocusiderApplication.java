56a47d8b99f9e201ddae32f61ad75a9078a752f8
ManchanTime <lych0918@gachon.ac.kr>
2024-08-15 02:44:44 +0900

    FB-11-feat Member 카테고리 추가

--- src/main/java/mono/focusider/FocusiderApplication.java
+++ src/main/java/mono/focusider/FocusiderApplication.java
@@ -2,7 +2,9 @@ package mono.focusider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class FocusiderApplication {
 

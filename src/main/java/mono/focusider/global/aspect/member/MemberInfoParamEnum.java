c56513ab895e4085ca9043df2de7e4ff04457892
ManchanTime <lych0918@gachon.ac.kr>
2024-08-15 02:44:24 +0900

    FB-11-feat ArgumentResolver를 이용하여 @MemberInfo 어노테이션을 이용한 엑세스토큰 파싱

---
+++ src/main/java/mono/focusider/global/aspect/member/MemberInfoParamEnum.java
@@ -0,0 +1,14 @@
+package mono.focusider.global.aspect.member;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public enum MemberInfoParamEnum {
+    MEMBER_ID("memberId"),
+    MEMBER_ROLE("memberRole");
+
+    private final String desc;
+}
--- src/main/java/mono/focusider/global/config/web/WebConfig.java
+++ src/main/java/mono/focusider/global/config/web/WebConfig.java
@@ -1,17 +1,24 @@
 package mono.focusider.global.config.web;
 
+import mono.focusider.global.aspect.member.MemberInfoParamHandlerArgumentResolver;
+import mono.focusider.global.security.JwtUtil;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final JwtUtil jwtUtil;
+
+    public WebConfig(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000") // Your Next.js app URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new MemberInfoParamHandlerArgumentResolver(jwtUtil));
     }
 }

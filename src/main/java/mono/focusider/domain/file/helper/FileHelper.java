8f78698da8206507762d1a06b640738a880792e3
ManchanTime <lych0918@gachon.ac.kr>
2024-08-16 02:20:47 +0900

    FB-12-feat File 테이블로 Member ProfileImage 저장

---
+++ src/main/java/mono/focusider/domain/file/helper/FileHelper.java
@@ -0,0 +1,34 @@
+package mono.focusider.domain.file.helper;
+
+import lombok.RequiredArgsConstructor;
+import mono.focusider.domain.file.domain.File;
+import mono.focusider.domain.file.repository.FileRepository;
+import mono.focusider.global.error.exception.EntityNotFoundException;
+import mono.focusider.global.utils.s3.FileType;
+import mono.focusider.global.utils.s3.S3Utils;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import static mono.focusider.domain.file.error.FileErrorCode.FILE_NOT_FOUND;
+
+@Component
+@RequiredArgsConstructor
+public class FileHelper {
+
+    private final S3Utils s3Utils;
+    private final FileRepository fileRepository;
+
+    public void deleteFile(String url) {
+        s3Utils.deleteFile(url);
+    }
+
+    public File uploadFile(MultipartFile file) {
+        String url = s3Utils.uploadFile(FileType.MEMBER_PROFILE_IMAGE, file);
+        return fileRepository.save(File.createFile(url, false));
+    }
+
+    public File findFileByUrl(String url) {
+        return fileRepository.findByUrl(url)
+                .orElseThrow(() -> new EntityNotFoundException(FILE_NOT_FOUND));
+    }
+}

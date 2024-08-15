41d23633c2f5788f644c3c240119b95e179b29fb
ManchanTime <lych0918@gachon.ac.kr>
2024-08-16 00:51:37 +0900

    FB-11-feat Member 레벨 설정 로직 추가

--- src/main/java/mono/focusider/domain/member/service/MemberService.java
+++ src/main/java/mono/focusider/domain/member/service/MemberService.java
@@ -3,17 +3,17 @@ package mono.focusider.domain.member.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mono.focusider.domain.category.domain.Category;
-import mono.focusider.domain.category.domain.MemberCategory;
 import mono.focusider.domain.category.helper.CategoryHelper;
 import mono.focusider.domain.category.mapper.MemberCategoryMapper;
 import mono.focusider.domain.member.domain.Member;
 import mono.focusider.domain.member.dto.req.MemberCategorySaveReq;
 import mono.focusider.domain.member.helper.MemberHelper;
+import mono.focusider.domain.member.type.ReadingHardType;
+import mono.focusider.domain.member.type.ReadingTermType;
 import mono.focusider.global.aspect.member.MemberInfoParam;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -29,9 +29,17 @@ public class MemberService {
     @Transactional
     public void createMemberCategory(MemberCategorySaveReq memberCategorySaveReq, MemberInfoParam memberInfo) {
         Member member = memberHelper.findMemberByIdOrThrow(memberInfo.memberId());
+        Integer level = settingLevel(memberCategorySaveReq);
         List<Category> categories = categoryHelper.findCategoryListWithType(memberCategorySaveReq.categoryTypes());
         categories.forEach(category -> {
             member.addMemberCategory(memberCategoryMapper.toMemberCategory(member, category));
         });
+        member.updateMemberLevel(level);
+    }
+
+    private Integer settingLevel(MemberCategorySaveReq memberCategorySaveReq) {
+        ReadingTermType readingTermType = memberCategorySaveReq.readingTermType();
+        ReadingHardType readingHardType = memberCategorySaveReq.readingHardType();
+        return (int) Math.ceil((double) (readingTermType.getCode() * readingHardType.getCode()) / 3);
     }
 }

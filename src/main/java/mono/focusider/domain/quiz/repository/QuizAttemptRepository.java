14b179c822bb2612b8d73513d4f233e9045d3762
ManchanTime <lych0918@gachon.ac.kr>
2024-08-17 15:56:59 +0900

    FB-13-feat Quiz 관련 도메인 repository 추가

---
+++ src/main/java/mono/focusider/domain/quiz/repository/QuizAttemptRepository.java
@@ -0,0 +1,6 @@
+package mono.focusider.domain.quiz.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuizAttemptRepository extends JpaRepository<QuizAttemptRepository, Long> {
+}

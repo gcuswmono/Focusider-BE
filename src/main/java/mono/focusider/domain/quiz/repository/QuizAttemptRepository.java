cf4b6f6b8247eedc4839e9cf0a9cf690db09c82d
ManchanTime <lych0918@gachon.ac.kr>
2024-08-21 18:19:28 +0900

    FB-17-feat 오답 재 풀이 채점 api

--- src/main/java/mono/focusider/domain/quiz/repository/QuizAttemptRepository.java
+++ src/main/java/mono/focusider/domain/quiz/repository/QuizAttemptRepository.java
@@ -3,5 +3,6 @@ package mono.focusider.domain.quiz.repository;
 import mono.focusider.domain.quiz.domain.QuizAttempt;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuizAttemptRepository extends JpaRepository<QuizAttempt, Long>, QuizAttemptWrongQueryRepository {
+public interface QuizAttemptRepository extends JpaRepository<QuizAttempt, Long>,
+        QuizAttemptWrongQueryRepository, QuizAttemptQueryRepository {
 }

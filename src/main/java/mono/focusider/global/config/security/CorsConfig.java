4346af44f8cf2eecdaea6258cd6b57e9b8ee6b7b
ManchanTime <lych0918@gachon.ac.kr>
2024-08-22 21:47:36 +0900

    hot-fix

--- src/main/java/mono/focusider/global/config/security/CorsConfig.java
+++ src/main/java/mono/focusider/global/config/security/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
                 .allowCredentials(true)
-                .allowedOrigins("http://localhost:3000", "http://localhost:3001", "3.38.179.245:3000", "focusider.shop")
+                .allowedOrigins("http://localhost:3000", "http://localhost:3001", "3.38.179.245:3000", "focusider.shop", "localhost.focusider")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
     }
 }
--- src/main/java/mono/focusider/global/utils/cookie/CookieUtils.java
+++ src/main/java/mono/focusider/global/utils/cookie/CookieUtils.java
@@ -40,6 +40,7 @@ public class CookieUtils {
         cookie.setPath("/");
         cookie.setHttpOnly(true);
         cookie.setMaxAge(maxAge);
+        cookie.setDomain("localhost");
         cookie.setAttribute("SameSite", "None");
         cookie.setSecure(false);
         response.addCookie(cookie);

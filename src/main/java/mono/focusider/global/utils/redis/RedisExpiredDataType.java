0349278f77e9931c169982ea685d2403df6f0139
ManchanTime <lych0918@gachon.ac.kr>
2024-08-24 15:08:49 +0900

    FB-23-feat Member Hard Delete 용 RedisExpiredListener 추가

--- src/main/java/mono/focusider/global/utils/redis/RedisExpiredDataType.java
+++ src/main/java/mono/focusider/global/utils/redis/RedisExpiredDataType.java
@@ -7,11 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public enum RedisExpiredDataType {
-    RAFFLE_WAIT_MEMBER_EXPIRED("raffle_wait_member:", "1", 86400000L),
-    RAFFLE_WIN_MEMBER_EXPIRED("raffle_win_member:", "3", 259200000L),
-    EMAIL_RECEIVED_MEMBER("certification:", null, 300000L),
-    PENALTY_RECEIVED_MEMBER("penalty:", null, 300000L),
-    WKT_END_EXPIRED("wkt_end_date:", null, null);
+    MEMBER_HARD_DELETE("member_deleted:", "1", 1209600000L);
 
     private final String prefix;
     private final String defaultValue;

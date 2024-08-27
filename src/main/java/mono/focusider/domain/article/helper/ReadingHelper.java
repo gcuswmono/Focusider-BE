a2c154055726efde69ec61ca80100c4e143706a7
ManchanTime <lych0918@gachon.ac.kr>
2024-08-27 14:11:09 +0900

    FB-28-fix 읽은 아티클 페이지로 변경 + 코드 리팩토링

--- src/main/java/mono/focusider/domain/article/helper/ReadingHelper.java
+++ src/main/java/mono/focusider/domain/article/helper/ReadingHelper.java
@@ -1,8 +1,12 @@
 package mono.focusider.domain.article.helper;
 
 import lombok.RequiredArgsConstructor;
+import mono.focusider.domain.article.dto.info.ReadingInfo;
 import mono.focusider.domain.article.dto.info.ReadingStatInfo;
+import mono.focusider.domain.article.dto.res.ReadingDetailResDto;
 import mono.focusider.domain.article.repository.ReadingRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -15,5 +19,13 @@ public class ReadingHelper {
 
     public List<ReadingStatInfo> findReadingStatInfo(Long memberId, LocalDate statDate) {
         return readingRepository.findReadingStatInfo(memberId, statDate);
+    }
+
+    public Page<ReadingInfo> findReadingInfos(Long memberId, Pageable pageable) {
+        return readingRepository.findReadingInfoPage(memberId, pageable);
+    }
+
+    public ReadingDetailResDto findReadingDetail(Long articleId) {
+        return readingRepository.findReadingDetail(articleId);
     }
 }
--- src/main/java/mono/focusider/domain/article/mapper/ReadingMapper.java
+++ src/main/java/mono/focusider/domain/article/mapper/ReadingMapper.java
@@ -1,7 +1,11 @@
 package mono.focusider.domain.article.mapper;
 
+import mono.focusider.domain.article.dto.info.ReadingInfo;
 import mono.focusider.domain.article.dto.info.ReadingStatInfo;
+import mono.focusider.domain.article.dto.res.ReadingPageResDto;
 import mono.focusider.domain.article.dto.res.ReadingStatResDto;
+import mono.focusider.global.domain.PageInfo;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -10,5 +14,10 @@ import java.util.List;
 public class ReadingMapper {
     public ReadingStatResDto toReadingStatDto(List<ReadingStatInfo> readingStatInfos) {
         return ReadingStatResDto.of(readingStatInfos);
+    }
+
+    public ReadingPageResDto toReadingPageResDto(Page<ReadingInfo> readingInfos) {
+        PageInfo pageInfo = PageInfo.of(readingInfos);
+        return ReadingPageResDto.of(readingInfos.getContent(), pageInfo);
     }
 }

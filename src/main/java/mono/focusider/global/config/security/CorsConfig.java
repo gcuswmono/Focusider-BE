953aa7642f7e743bf21ea371cae85f40273edac1
ManchanTime <lych0918@gachon.ac.kr>
2024-08-28 17:34:40 +0900

    hot-fix

--- src/main/java/mono/focusider/global/config/security/CorsConfig.java
+++ src/main/java/mono/focusider/global/config/security/CorsConfig.java
@@ -1,14 +1,8 @@
 package mono.focusider.global.config.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
@@ -21,24 +15,5 @@ public class CorsConfig implements WebMvcConfigurer {
                         "http://focusider.shop:3000", "http://focusider.shop", "https://focusider.shop",
                         "http://localhost.focusider:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000", "http://localhost:3001",
-                "http://3.38.179.245:3000", "http://focusider.shop:3000",
-                "http://focusider.shop", "https://focusider.shop",
-                "http://localhost.focusider:3000"
-        ));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }

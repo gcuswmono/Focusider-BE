a657e99e085425f9a6c66ba48ef2bf8f2067222a
ManchanTime <lych0918@gachon.ac.kr>
2024-08-28 17:29:01 +0900

    hot-fix

--- src/main/java/mono/focusider/global/config/security/CorsConfig.java
+++ src/main/java/mono/focusider/global/config/security/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
                 .allowCredentials(true)
+                .allowedHeaders("*")
                 .allowedOrigins("http://localhost:3000", "http://localhost:3001", "http://3.38.179.245:3000",
                         "http://focusider.shop:3000", "http://focusider.shop", "https://focusider.shop",
                         "http://localhost.focusider:3000")
